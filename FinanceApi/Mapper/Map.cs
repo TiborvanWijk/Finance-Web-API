c104c6e3009058bc649a875e470f877b1daf32e7
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-01-01 12:47:06 +0100

    All mappings + set users currency to regional default

--- FinanceApi/Mapper/Map.cs
+++ FinanceApi/Mapper/Map.cs
@@ -6,7 +6,19 @@ namespace FinanceApi.Mapper
     public abstract class Map
     {
 
-        public static UserDto ToUserDto(User user)
+        internal static User ToUser(UserDto userDto)
+        {
+            var user = new User()
+            {
+                Id = userDto.Id,
+                UserName = userDto.Username,
+                Balance = userDto.Balance,
+                Currency = userDto.Currency,
+            };
+            return user;
+        }
+
+        internal static UserDto ToUserDto(User user)
         {
             var userDto = new UserDto()
             {
@@ -50,5 +62,121 @@ namespace FinanceApi.Mapper
             };
             return incomeDto;
         }
+
+        internal static Expense ToExpense(ExpenseDto expenseDto)
+        {
+            var expense = new Expense()
+            {
+                Id = expenseDto.Id,
+                Title = expenseDto.Title,
+                Description = expenseDto.Description,
+                Urgency = expenseDto.Urgency,
+                DocumentUrl = expenseDto.DocumentUrl,
+                Status = expenseDto.Status,
+                Date = expenseDto.Date,
+                Currency = expenseDto.Currency,
+                Amount = expenseDto.Amount,
+            };
+            return expense;
+        }
+
+
+        internal static ExpenseDto ToExpenseDto(Expense expense)
+        {
+            var expenseDto = new ExpenseDto()
+            {
+                Id = expense.Id,
+                Title = expense.Title,
+                Description = expense.Description,
+                Urgency = expense.Urgency,
+                DocumentUrl = expense.DocumentUrl,
+                Status = expense.Status,
+                Date = expense.Date,
+                Currency = expense.Currency,
+                Amount = expense.Amount,
+            };
+            return expenseDto;
+        }
+
+        internal static Budget ToBudget(BudgetDto budgetDto)
+        {
+            var budget = new Budget()
+            {
+                Id = budgetDto.Id,
+                Title = budgetDto.Title,
+                Description = budgetDto.Description,
+                LimitAmount = budgetDto.LimitAmount,
+                Urgency = budgetDto.Urgency,
+                StartDate = budgetDto.StartDate,
+                EndDate = budgetDto.EndDate,
+            };
+            return budget;
+        }
+
+        internal static BudgetDto ToBudgetDto(Budget budget)
+        {
+            var budgetDto = new BudgetDto()
+            {
+                Id = budget.Id,
+                Title = budget.Title,
+                Description = budget.Description,
+                LimitAmount = budget.LimitAmount,
+                Urgency = budget.Urgency,
+                StartDate = budget.StartDate,
+                EndDate = budget.EndDate,
+            };
+            return budgetDto;
+        }
+
+        internal static Goal ToGoal(GoalDto goalDto)
+        {
+            var goal = new Goal()
+            {
+                Id = goalDto.Id,
+                Title = goalDto.Title,
+                Description = goalDto.Description,
+                Amount = goalDto.Amount,
+                Progress = goalDto.Progress,
+                TargetDate = goalDto.TargetDate,
+            };
+            return goal;
+        }
+
+        internal static GoalDto ToGoalDto(Goal goal)
+        {
+            var goalDto = new GoalDto()
+            {
+                Id = goal.Id,
+                Title = goal.Title,
+                Description = goal.Description,
+                Amount = goal.Amount,
+                Progress = goal.Progress,
+                TargetDate = goal.TargetDate,
+            };
+            return goalDto;
+        }
+
+        internal static Category ToCategory(CategoryDto categoryDto)
+        {
+            var category = new Category()
+            {
+                Id = categoryDto.Id,
+                Title = categoryDto.Title,
+                Description = categoryDto.Description,
+            };
+            return category;
+        }
+
+        internal static CategoryDto ToCategoryDto(Category category)
+        {
+            var categoryDto = new CategoryDto()
+            {
+                Id = category.Id,
+                Title = category.Title,
+                Description = category.Description,
+            };
+            return categoryDto;
+        }
+
     }
 }
--- FinanceApi/Models/User.cs
+++ FinanceApi/Models/User.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Globalization;
 
 namespace FinanceApi.Models
 {
     public class User : IdentityUser
     {
         public decimal Balance { get; set; } = 0;
-        public string Currency { get; set; } = "EUR";
+        public string Currency { get; set; } = RegionInfo.CurrentRegion.ISOCurrencySymbol;
         public ICollection<Expense> Expenses{ get; set; }
         public ICollection<Income> Incomes { get; set; }
         public ICollection<Budget> Budgets { get; set; }
