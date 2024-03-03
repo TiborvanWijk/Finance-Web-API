92b47ce320e02e875554b8e1d1c3157cac93b462
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-03-03 21:40:07 +0100

    Category integration setup

---
+++ FinanceApi.Test/IntegrationTests/CategoryEndpointsIntegrationTests.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanceApi.Test.Utils;
+
+namespace FinanceApi.Test.IntegrationTests
+{
+    public class CategoryEndpointsIntegrationTests : IDisposable
+    {
+        private CustomWebApplicationFactory factory;
+        private HttpClient client;
+        public CategoryEndpointsIntegrationTests()
+        {
+            factory = new CustomWebApplicationFactory();
+            client = factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+            factory.Dispose();
+        }
+
+
+        public async Task<string> GetAuthenticationTokenAsync(string email, string password)
+        {
+            var requestBody = new StringContent(
+                JsonConvert.SerializeObject(new
+                {
+                    email,
+                    password
+                }),
+                Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("/login", requestBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<object>(await response.Content.ReadAsStringAsync());
+                string accesToken = JObject.FromObject(data)["accessToken"]?.ToString();
+                return accesToken;
+            }
+
+
+            return null;
+        }
+    }
+}
